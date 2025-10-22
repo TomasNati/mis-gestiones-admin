133f89dcfa74d6b799e6d980b69709393971fcf5
Andres Asteasuain <andres.asteasuain@hotmail.com>
2025-10-22 19:06:43 -0300

    Fixing details

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,22 @@
-import axios from 'axios';
-import { type Categoria, type Subcategoria } from '../model/types';
+import axios from "axios";
+import { type Categoria, type Subcategoria } from "../model/types";
 const backendAPI = import.meta.env.VITE_BACKEND_API;
-
-
-console.log(`Connecting to ${backendAPI}`);
 
 const apiClient = axios.create({
   baseURL: backendAPI,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 export const fetchCategorias = async () => {
-  const response = await apiClient.get<Categoria[]>('/categorias');
+  const response = await apiClient.get<Categoria[]>("/categorias");
   return response.data;
 };
 
 export const fetchSubcategorias = async (categoriaId: string) => {
-  const response = await apiClient.get<Subcategoria[]>(`/categorias/${categoriaId}/subcategorias`);
+  const response = await apiClient.get<Subcategoria[]>(
+    `/categorias/${categoriaId}/subcategorias`,
+  );
   return response.data;
 };
