522680474d7598b0267c71312af502aa6918cd84
Andres Asteasuain <andres.asteasuain@hotmail.com>
2025-11-09 23:09:22 -0300

    For now, fixing the model

--- src/model/models.ts
+++ src/model/models.ts
@@ -13,10 +13,9 @@ export const categoriaEditSchema = categoriaBaseSchema.extend({
 export const subcategoriaBaseSchema = z.object({
   nombre: z.string().trim().min(1, "Nombre es requerido"),
   comentarios: z.string().optional().nullable(),
-  categoria: categoriaEditSchema
-    .optional()
-    .nullable()
-    .refine((val) => !!val, { message: "Categoría es requerida" }),
+  categoria: categoriaEditSchema.refine((val) => !!val, {
+    message: "Categoría es requerida",
+  }),
 });
 
 export const subcategoriaEditSchema = subcategoriaBaseSchema.extend({
